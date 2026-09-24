213f409b19bec8d8825e527d1bcb74f8eebb43da
Admin <Admin@DESKTOP-KF6T59B>
2026-09-24 16:22:06 +0530

    employee details

---
+++ SamplePracticetestproject/src/test/java/com/practice/employeeTest/EmployeeTest.java
@@ -0,0 +1,21 @@
+package com.practice.employeeTest;
+
+import org.testng.annotations.Test;
+
+public class EmployeeTest {
+
+
+
+	    @Test
+	    public void addEmployeeTest() {
+	        System.out.println("Employee added successfully");
+	    }
+
+	    @Test
+	    public void updateEmployeeTest() {
+	        System.out.println(" All Employee details updated successfully");
+	    }
+
+	}
+
+
