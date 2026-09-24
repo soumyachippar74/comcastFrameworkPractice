3b319acd48c641939cf2941a07d59dab09ac1079
Admin <Admin@DESKTOP-KF6T59B>
2026-09-24 17:20:43 +0530

    added employee id and updated successfully

--- SamplePracticetestproject/src/test/java/com/practice/employeeTest/EmployeeTest.java
+++ SamplePracticetestproject/src/test/java/com/practice/employeeTest/EmployeeTest.java
@@ -9,11 +9,13 @@ public class EmployeeTest {
 	    @Test
 	    public void addEmployeeTest() {
 	        System.out.println("Employee added successfully");
+	        System.out.println("Employee ID: EMP101");
 	    }
 
 	    @Test
 	    public void updateEmployeeTest() {
 	        System.out.println(" All Employee details updated successfully");
+	        System.out.println("Employee salary updated successfully");
 	    }
 
 	}
